56cc2ab5e12f97129b0c8408c87728dd853c0439
lopezito1 <mee.diiceen.miky@gmail.com>
2026-03-02 21:14:18 -0400

    Modelo Articulo creado

---
+++ Models/Articulo.cs
@@ -0,0 +1,10 @@
+namespace BarrioTecApp.Models
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
